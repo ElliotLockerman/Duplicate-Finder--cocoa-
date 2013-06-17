a3dca970b9416f2c1afcd32a555f251971c919d9
Elliot Lockerman <elliot.lockerman.info>
2013-06-17 10:19:42 -0400

    Folder to search box and button both work

---
+++ Duplicate Finder (cocoa)/ELCentralController.h
@@ -0,0 +1,13 @@
+//
+//  ELController.h
+//  Duplicate Finder (cocoa)
+//
+//  Created by E L on 6/17/13.
+//  Copyright (c) 2013 n/a. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ELController : NSObject
+
+@end
---
+++ Duplicate Finder (cocoa)/ELCentralController.m
@@ -0,0 +1,13 @@
+//
+//  ELController.m
+//  Duplicate Finder (cocoa)
+//
+//  Created by E L on 6/17/13.
+//  Copyright (c) 2013 n/a. All rights reserved.
+//
+
+#import "ELController.h"
+
+@implementation ELController
+
+@end
