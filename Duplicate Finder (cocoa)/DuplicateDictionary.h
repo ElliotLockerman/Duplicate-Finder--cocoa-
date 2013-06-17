63975a0ddbe70e00c7366654e1470e6348d36775
Elliot Lockerman <elliot.lockerman.info>
2013-06-16 22:46:10 -0400

    Select folder dialog opens.

--- Duplicate Finder (cocoa)/DuplicateDictionary.h
+++ Duplicate Finder (cocoa)/DuplicateDictionary.h
@@ -14,6 +14,7 @@
 @property NSOpenPanel *pathOfFolderToSearch;
 @property NSIndexSet *filesToIgnore;
 
+- (IBAction)openExistingDocument:(id)sender;
 -(void) generateDictionary;
 -(void) displayDictionary;
 
--- Duplicate Finder (cocoa)/DuplicateDictionary.m
+++ Duplicate Finder (cocoa)/DuplicateDictionary.m
@@ -11,9 +11,23 @@
 @implementation DuplicateDictionary
 
 @synthesize duplicateDictionary;
-@synthesize pathOfFolderToSearch;
 @synthesize filesToIgnore;
 
+- (IBAction)openExistingDocument:(id)sender {
+    NSOpenPanel* panel = [NSOpenPanel openPanel];
+    
+    // This method displays the panel and returns immediately.
+    // The completion handler is called when the user selects an
+    // item or cancels the panel.
+    [panel beginWithCompletionHandler:^(NSInteger result){
+        if (result == NSFileHandlingPanelOKButton) {
+            NSURL*  theDoc = [[panel URLs] objectAtIndex:0];
+            
+            // Open  the document.
+        }
+        
+    }];
+}
 -(void)generateDictionary
 {
     
