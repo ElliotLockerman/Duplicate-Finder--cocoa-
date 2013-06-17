8f3257d20ffdd2d84df93ce5f35d0ceaa191c7d7
Elliot Lockerman <elliot.lockerman.info>
2013-06-16 22:37:12 -0400

    Progress

---
+++ Duplicate Finder (cocoa)/DuplicateDictionary.h
@@ -0,0 +1,20 @@
+//
+//  duplicateDictionary.h
+//  Duplicate Finder (cocoa)
+//
+//  Created by E L on 6/16/13.
+//  Copyright (c) 2013 n/a. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DuplicateDictionary : NSObject
+
+@property NSDictionary *duplicateDictionary;
+@property NSOpenPanel *pathOfFolderToSearch;
+@property NSIndexSet *filesToIgnore;
+
+-(void) generateDictionary;
+-(void) displayDictionary;
+
+@end
---
+++ Duplicate Finder (cocoa)/DuplicateDictionary.m
@@ -0,0 +1,22 @@
+//
+//  duplicateDictionary.m
+//  Duplicate Finder (cocoa)
+//
+//  Created by E L on 6/16/13.
+//  Copyright (c) 2013 n/a. All rights reserved.
+//
+
+#import "DuplicateDictionary.h"
+
+@implementation DuplicateDictionary
+
+@synthesize duplicateDictionary;
+@synthesize pathOfFolderToSearch;
+@synthesize filesToIgnore;
+
+-(void)generateDictionary
+{
+    
+}
+
+@end
